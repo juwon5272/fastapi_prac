f0d4e0e6ff275e69ea183fb1111171ee80cbac46
juwon5272 <jackson5272@naver.com>
2023-08-21 23:36:41 +0900

    feat : test main.py

---
+++ venvs/myapi/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요 파이보"}
+
+# uvicorn main:app --reload
