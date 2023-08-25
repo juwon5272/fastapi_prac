8ca57483fc164422405ef5be39e586bbac1e4d01
juwon5272 <jackson5272@naver.com>
2023-08-25 18:49:03 +0900

    feat : 질문 작성시 작상자도 저장

--- venvs/myapi/domain/question/question_schema.py
+++ venvs/myapi/domain/question/question_schema.py
@@ -3,6 +3,7 @@ import datetime
 from pydantic import BaseModel, validator
 
 from domain.answer.answer_schema import Answer
+from domain.user.user_schema import User
 
 
 class Question(BaseModel):
@@ -11,6 +12,7 @@ class Question(BaseModel):
     content: str
     create_date: datetime.datetime
     answers: list[Answer] = []
+    user: User | None
 
     class Config:
         orm_mode = True
@@ -24,3 +26,7 @@ class QuestionCreate(BaseModel):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
         return v
+
+class QuestionList(BaseModel):
+    total: int = 0
+    question_list: list[Question] = []
